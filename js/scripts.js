b998831baf32639378be8848f03312b36f88d221
nv <nv781@yahoo.com>
2017-08-10 07:52:04 -0700

    update page to be functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,17 @@
-$(document).ready(function(event){
+$(document).ready(function(){
 
-  $("#onlineMarket").submit(function(){
-    var fn = $("#firstName").val();
-    var ln = $("#lastName").val();
-    var addy = $("#address").val();
-    var city1 = $("#city").val();
-    // var state1 = $("#state").val();
-    var zipCode = $("#zip").val();
+  $("#onlineMarket").submit(function(event){
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var address = $("#address").val();
+    var city = $("#city").val();
+    var state = $("#state").val();
+    var zip = $("#zip").val();
 
     var $output = $(".output");
     $("#onlineMarket").hide();
-    $output.text("Thank you " + fn + " " + ln + " for your purchase!");
+    $output.text("Thank you " + firstName + " " + lastName + " for your purchase!");
 
     event.preventDefault();
   });
 });
-
-
-
-
-
-
-
-
-// $("#name").submit(function(e){
-//      var person = $("#person").val();
-//      var description = $("#description").val();
-//      var date = $("#date").val();
-//      var newDate = new Date(date);
-//      var time = $("#time").val();
-//      var $output = $(".output");
-//      $("#name").hide();
-//      $output.text("Thank you " + person + " for setting an appointment on " + newDate + " at " + time + ". I will get back to you as soon as possible!");
-//
-//      e.preventDefault();
-//    });
